ec2c1438823bed8f0a81a40335a2a7bfc2b73710
Mirabbos <mirabbosahmedov7777@gmail.com>
2024-04-24 23:07:57 +0500

    4 files edited

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
+        int third = 0;
+        for (int i = 1; i <= lastPrinted; i++) {
+            third++;
+            if (third % 3 == 0) {
 
+            }else {
+                System.out.println(i);
+            }
+        }
     }
 }
